f3e0d360d7dbcae319a17df075bae1c89b5c66c8
rayburgemeestre <ray.burgemeestre@gmail.com>
2015-12-25 22:18:46 +0100

    Remove responsibility for generating from job_storage -> job_generator.

--- src/job_generator.cpp
+++ src/job_generator.cpp
@@ -6,17 +6,28 @@ size_t current_frame = 0;
 behavior job_generator(event_based_actor* self, const caf::actor &job_storage) {
     return {
         [=](prepare_frame) {
-            // create a frame
-            std::this_thread::sleep_for(std::chrono::milliseconds(20));
-            // send it to storage
-            self->sync_send(job_storage, add_job_atom::value, current_frame++, false).then(
-                [=](no_more_jobs_atom) {
-                    aout(self) << "job_generator: there are no more jobs to be generated." << endl;
-                    self->become(nothing);
-                },
+            self->sync_send(job_storage, num_jobs_atom::value).then(
                 [=](num_jobs_atom, unsigned long numjobs) {
+                    // Renderer has some catching up to do
                     if (numjobs >= desired_num_jobs_queued) {
+                        // retry in 200 milliseconds
                         std::this_thread::sleep_for(std::chrono::milliseconds(200));
+                        self->send(self, prepare_frame::value);
+                        return;
+                    }
+
+                    // Create a frame
+                    aout(self) << "job_generator: generated frame #" << current_frame << endl;
+                    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+
+                    // Store it
+                    self->send(job_storage, add_job_atom::value, current_frame++, false);
+
+                    // Our imaginary video is only 25 frames... so we are done
+                    if (current_frame >= 25) {
+                        aout(self) << "job_generator: there are no more jobs to be generated." << endl;
+                        self->become(nothing);
+                        return;
                     }
                     self->send(self, prepare_frame::value);
                 }
--- src/job_storage.cpp
+++ src/job_storage.cpp
@@ -15,22 +15,16 @@ behavior job_storage(event_based_actor* self) {
             }
             return make_message(get_job_atom::value, *it);
         },
-        [=](add_job_atom, size_t frame, bool rendered) -> message {
-            if (frame > 25) {
-                return make_message(no_more_jobs_atom::value);
-            }
+        [=](add_job_atom, size_t frame, bool rendered) {
             data::job new_job;
             new_job.frame = frame;
             new_job.rendered = rendered;
             jobs.push_back(new_job);
-            aout(self) << "job_storage: prepared frame: " << new_job.frame << endl;
-            return make_message(num_jobs_atom::value, jobs.size());
         },
-        [=](remove_job_atom, size_t jobdone) -> message {
+        [=](remove_job_atom, size_t jobdone) {
             jobs.erase( std::remove_if(jobs.begin(), jobs.end(), [&jobdone](struct data::job j) {
                 return j.frame == jobdone;
             }), jobs.end() );
-            return make_message(num_jobs_atom::value, jobs.size());
         },
         [=](num_jobs_atom) -> message {
             return make_message(num_jobs_atom::value, jobs.size());
