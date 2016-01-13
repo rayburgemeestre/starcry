c9eb9b73406ac8fc4064914e9c59960358760a13
rayburgemeestre <ray.burgemeestre@gmail.com>
2016-01-13 23:53:19 +0100

    Reformat code.

--- src/announce.cpp
+++ src/announce.cpp
@@ -10,32 +10,40 @@ using namespace data;
 
 namespace data {
     void announce() {
-        caf::announce<job>("job",
-                           &job::width,
-                           &job::height,
-                           &job::offset_x,
-                           &job::offset_y,
-                           &job::job_number,
-                           &job::frame_number,
-                           &job::rendered,
-                           &job::last_frame,
-                           &job::chunk,
-                           &job::num_chunks,
-                           &job::shapes);
-        caf::announce<shape>("shape",
-                             &shape::x,
-                             &shape::y,
-                             &shape::z,
-                             &shape::type,
-                             &shape::radius,
-                             &shape::radius_size);
-        caf::announce<ALLEGRO_COLOR>("ALLEGRO_COLOR",
-                                     &ALLEGRO_COLOR::r,
-                                     &ALLEGRO_COLOR::g,
-                                     &ALLEGRO_COLOR::b,
-                                     &ALLEGRO_COLOR::a);
-        caf::announce<pixel_data>("pixel_data",
-                                  &pixel_data::pixels);
+        caf::announce<job>(
+            "job",
+            &job::width,
+            &job::height,
+            &job::offset_x,
+            &job::offset_y,
+            &job::job_number,
+            &job::frame_number,
+            &job::rendered,
+            &job::last_frame,
+            &job::chunk,
+            &job::num_chunks,
+            &job::shapes
+        );
+        caf::announce<shape>(
+            "shape",
+            &shape::x,
+            &shape::y,
+            &shape::z,
+            &shape::type,
+            &shape::radius,
+            &shape::radius_size
+        );
+        caf::announce<ALLEGRO_COLOR>(
+            "ALLEGRO_COLOR",
+            &ALLEGRO_COLOR::r,
+            &ALLEGRO_COLOR::g,
+            &ALLEGRO_COLOR::b,
+            &ALLEGRO_COLOR::a
+        );
+        caf::announce<pixel_data>(
+            "pixel_data",
+            &pixel_data::pixels
+        );
     }
 
 }
