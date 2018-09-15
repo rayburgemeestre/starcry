91a821a760050f6b8114329eac6ea0b030cdd61f
rayburgemeestre <ray.burgemeestre@gmail.com>
2018-09-15 19:36:37 +0200

    These Dockerfile's work for Ubuntu 16.04.

--- docker/build_Ubuntu16.04.sh
+++ docker/build_Ubuntu16.04.sh
@@ -2,7 +2,7 @@
 
 #docker build --no-cache -t sc_build_ubuntu:16.04 ./Ubuntu16.04
 
-docker build -t sc_build_ubuntu:16.04 ./Ubuntu16.04
+docker build "$@" -t sc_build_ubuntu:16.04 ./Ubuntu16.04
 
 echo docker login
 echo docker tag sc_build_ubuntu:16.04 rayburgemeestre/sc_build_ubuntu:16.04
