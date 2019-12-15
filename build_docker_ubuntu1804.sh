5d35bfd5fe9607d65f151d23657720ed14cde652
rayburgemeestre <ray.burgemeestre@gmail.com>
2019-12-15 14:17:14 +0100

    Remove -i flag for CI build

--- build_docker_ubuntu1804.sh
+++ build_docker_ubuntu1804.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -i -t -v $PWD:/projects/starcry rayburgemeestre/sc_build_ubuntu:18.04 sh -c "$*"
+docker run -t -v $PWD:/projects/starcry rayburgemeestre/sc_build_ubuntu:18.04 sh -c "$*"
 
