5ae956b363b02cdcd165722d0d20e0d11fe56499
rayburgemeestre <ray.burgemeestre@gmail.com>
2020-07-11 12:26:56 +0200

    Remove deprecated code

--- deprecated/examples/tcpdump_hadoop/README.md
+++
@@ -1,8 +0,0 @@
-Scripts used:
-
-- gunzip tcpdump\_all.txt.gz
-- inspect.sh , tcpdump\_all -> generates tcpdump.sh
-- hand edited that into: tcpdump2.sh -> generates tcpdump\_all.txt
-- cat tcpdump\_all.txt| ./starcry --stdin -d 1920x1080 -n 8 -c 1
-
-Result: https://www.youtube.com/watch?v=rGXajJ4XqKg
--- deprecated/util/remote_actors.h
+++
@@ -1,9 +0,0 @@
-#include <string>
-
-bool connect_remote_worker(actor_system &system,
-                           const std::string &actor_name,
-                           const std::string &host,
-                           const int &port,
-                           std::optional<actor> *actor_ptr);
-
-bool publish_remote_actor(const std::string &actor_name, event_based_actor *self, int port);
