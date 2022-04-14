0ff555f0204744668e705eccef684389b8f5d805
Ray Burgemeestre <ray@cppse.nl>
2022-04-14 07:51:50 +0200

    Make entrypoint script less verbose.

--- docs/entrypoint.sh
+++ docs/entrypoint.sh
@@ -6,11 +6,11 @@ set -o pipefail
 if [[ "$_UID" != "" ]]; then
   groupadd -g $_GID user || true
   useradd -r -u $_UID -g $_GID user || true
-  usermod -a -G wheel user || true
+  usermod -a -G wheel user 1>/dev/null 2>& || true
   mkdir -p /home/user
   chown $_UID:$_GID /home/user
   echo 'user ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-  set +ex
+  set +e
   echo 'export PATH=/usr/local/bin:$PATH' | sudo tee -a /home/user/.bashrc
   echo chown $_UID:$_GID /home/user/.bashrc
   chown $_UID:$_GID /home/user/.bashrc /tmp/.ccache /tmp/.emscripten_cache
@@ -18,7 +18,6 @@ if [[ "$_UID" != "" ]]; then
   ln -sf /tmp/.emscripten_cache /home/user/.emscripten_cache
   sudo -u $(id -u -n $_UID) -g $(id -g -n $_UID) /bin/bash "$@"
 else
-    echo here
   echo 'export PATH=/usr/local/bin:$PATH' | sudo tee -a /root/.bashrc
   /bin/bash "$@"
 fi
