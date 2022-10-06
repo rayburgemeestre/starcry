da444ca1fcedbd2d9e3a3d070f55786c769b103e
Ray Burgemeestre <ray@cppse.nl>
2022-10-06 22:10:16 +0000

    Move callback helpers to shape.hpp

--- src/data_staging/shape.hpp
+++ src/data_staging/shape.hpp
@@ -24,3 +24,43 @@ namespace data_staging {
 using shape_t =
     std::variant<std::monostate, data_staging::circle, data_staging::line, data_staging::text, data_staging::script>;
 }
+
+void meta_visit(auto& shape, auto&& handle_circle, auto&& handle_line, auto&& handle_text, auto&& handle_script) {
+  std::visit(overloaded{[](std::monostate) {}, handle_circle, handle_line, handle_text, handle_script}, shape);
+}
+
+template <typename T>
+void meta_callback(T& shape, auto&& callback) {
+  meta_visit(
+      shape,
+      [&callback](data_staging::circle& c) {
+        callback(c);
+      },
+      [&callback](data_staging::line& l) {
+        callback(l);
+      },
+      [&callback](data_staging::text& t) {
+        callback(t);
+      },
+      [&callback](data_staging::script& s) {
+        callback(s);
+      });
+}
+
+template <typename T>
+void meta_callback(const T& shape, auto&& callback) {
+  meta_visit(
+      shape,
+      [&callback](const data_staging::circle& c) {
+        callback(c);
+      },
+      [&callback](const data_staging::line& l) {
+        callback(l);
+      },
+      [&callback](const data_staging::text& t) {
+        callback(t);
+      },
+      [&callback](const data_staging::script& s) {
+        callback(s);
+      });
+}
--- src/generator.cpp
+++ src/generator.cpp
@@ -24,50 +24,10 @@
 #include "util/vector_logic.hpp"
 
 #include "data/texture.hpp"
-#include "data_staging/circle.hpp"
+
 #include "shapes/circle.h"
 #include "shapes/position.h"
 #include "shapes/rectangle.h"
-
-void meta_visit(auto& shape, auto&& handle_circle, auto&& handle_line, auto&& handle_text, auto&& handle_script) {
-  std::visit(overloaded{[](std::monostate) {}, handle_circle, handle_line, handle_text, handle_script}, shape);
-}
-
-template <typename T>
-void meta_callback(T& shape, auto&& callback) {
-  meta_visit(
-      shape,
-      [&callback](data_staging::circle& c) {
-        callback(c);
-      },
-      [&callback](data_staging::line& l) {
-        callback(l);
-      },
-      [&callback](data_staging::text& t) {
-        callback(t);
-      },
-      [&callback](data_staging::script& s) {
-        callback(s);
-      });
-}
-
-template <typename T>
-void meta_callback(const T& shape, auto&& callback) {
-  meta_visit(
-      shape,
-      [&callback](const data_staging::circle& c) {
-        callback(c);
-      },
-      [&callback](const data_staging::line& l) {
-        callback(l);
-      },
-      [&callback](const data_staging::text& t) {
-        callback(t);
-      },
-      [&callback](const data_staging::script& s) {
-        callback(s);
-      });
-}
 
 void fix_properties(std::vector<std::vector<data_staging::shape_t>>& scene_shapes) {
   for (auto& shapes : scene_shapes) {
