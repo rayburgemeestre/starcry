b1151762089cf30d740e874a4f5ff372343bab3f
Ray Burgemeestre <ray@cppse.nl>
2022-12-27 18:58:07 +0000

    Add script that helped me track down a 'memory leak', turned out to be simple math: 1920x1080 pixels * 4 bytes per channel * 4 channels * 16 number of threads * 30 buffered frames for motion blur. == a lot of memory usage.

---
+++ input/memory_leak.js
@@ -0,0 +1,55 @@
+_ = {
+  'gradients': {
+    'ce475a6c-2427-420c-85de-6316f3027313': [
+      {'position': 0, 'r': 1, 'g': 0, 'b': 0, 'a': 1},
+      {'position': 1, 'r': 1, 'g': 0, 'b': 0, 'a': 0},
+    ],
+    'cd35b59b-bae9-4bef-b689-dcc8720e6be9': [
+      {'position': 0, 'r': 0, 'g': 1, 'b': 0, 'a': 1},
+      {'position': 1, 'r': 0, 'g': 1, 'b': 0, 'a': 0},
+    ],
+    '64f1da9f-07b2-46bd-8f62-30f9534f0cfd': [
+      {'position': 0, 'r': 0, 'g': 0, 'b': 1, 'a': 1},
+      {'position': 1, 'r': 0, 'g': 0, 'b': 1, 'a': 0},
+    ]
+  },
+  'objects': {
+    'test': {
+      'type': 'circle',
+      'gradient': 'ce475a6c-2427-420c-85de-6316f3027313',
+      'radius': 0,
+      'radiussize': 1920.0 * 2,
+      'blending_type': blending_type.normal,
+    },
+  },
+  'video': {
+    'fps': 25,
+    'width': 1920,
+    'height': 1080,
+    'scale': 1,
+    'rand_seed': 5,
+    'granularity': 1,
+    'minimize_steps_per_object': false,
+    'bg_color': {'r': 0., 'g': 0., 'b': 0., 'a': 1},
+    'grain_for_opacity': false,
+    'min_intermediates': 30,
+    'max_intermediates': 30,
+  },
+  'preview': {
+    'width': 512,
+    'height': 512,
+  },
+  'scenes': [
+    {
+      'name': 'scene1',
+      'duration': 1.0,
+      'objects': [
+        {'id': 'test', 'x': 0, 'y': 0, 'z': 0, 'props': {}},
+        {'id': 'test', 'x': 0, 'y': 0, 'z': 0, 'props': {}},
+        {'id': 'test', 'x': 0, 'y': 0, 'z': 0, 'props': {}},
+        {'id': 'test', 'x': 0, 'y': 0, 'z': 0, 'props': {}},
+        {'id': 'test', 'x': 0, 'y': 0, 'z': 0, 'props': {}},
+      ]
+    },
+  ]
+};
