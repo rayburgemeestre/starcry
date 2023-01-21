f021fee783db920596cc480e664ad4ef9b7e1b18
Ray Burgemeestre <ray@cppse.nl>
2023-01-21 23:09:24 +0100

    Script page loads script via API and makes sure Javascript syntax highlighting is used.

--- web2/src/pages/ScriptPage.vue
+++ web2/src/pages/ScriptPage.vue
@@ -1,18 +1,41 @@
 <template>
-  <monaco-editor name="container" v-model="script" />
+  <monaco-editor name="container" :value="script" language="javascript" />
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import MonacoEditor from 'components/MonacoEditor.vue';
+import { StarcryAPI } from 'components/api';
+
+let script = ref('');
+const script_endpoint = new StarcryAPI(
+  'script',
+  StarcryAPI.text_type,
+  (msg: string) => {
+    console.log(msg);
+  },
+  (buffer: string) => {
+    if (buffer[0] == '1') {
+      script_endpoint.send('open ' + buffer.slice(1));
+    } else if (buffer[0] == '2') {
+      script.value = buffer.slice(1);
+    } else if (buffer[0] == '3') {
+    } else if (buffer[0] == '4') {
+      // script listing
+    }
+  }
+);
+
 export default defineComponent({
   name: 'ScriptPage',
   components: {
     MonacoEditor,
   },
   setup() {
-    const script = ref('Hello world!');
     return { script };
+  },
+  mounted() {
+    script_endpoint.connect();
   },
 });
 </script>
