a1f930946e25d78b0b19aee6ee626211525e3881
Ray Burgemeestre <ray@cppse.nl>
2023-02-12 20:23:16 +0000

    Display objects in ObjectsPage as a table and suppress some ESLint warnings

--- web2/src/pages/ObjectsPage.vue
+++ web2/src/pages/ObjectsPage.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>{{ objects_store.objects }}</div>
+  <q-table
+    dense
+    :rows="rows(objects_store.objects)"
+    :columns="columns(objects_store.objects)"
+    row-key="#"
+    hide-header
+    hide-pagination
+    :rows-per-page-options="[100]"
+  >
+  </q-table>
 </template>
 
 <script lang="ts">
@@ -9,21 +18,61 @@ import { useObjectsStore } from 'stores/objects';
 export default defineComponent({
   name: 'ObjectsPage',
   setup() {
+    function columns(objects) {
+      let columns = [];
+      let index = 0;
+      if (objects.length === 0) {
+        return columns;
+      }
+      for (let key in objects[0]) {
+        columns.push({
+          name: key,
+          field: index,
+          align: 'left',
+          sortable: true,
+        });
+        index++;
+      }
+      console.log(columns);
+      return columns;
+    }
+    function rows(objects) {
+      let rows = [];
+      for (let object of objects) {
+        let row = [];
+        for (let property in object) {
+          row.push(object[property]);
+        }
+        rows.push(row);
+      }
+      console.log(rows);
+      return rows;
+    }
     let objects_store = useObjectsStore();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const objects_endpoint = new StarcryAPI(
       'objects',
       StarcryAPI.json_type,
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
       (msg) => {},
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       (buffer) => {
         objects_store.objects = buffer;
         // this.render_objects();
       },
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
       (_) => {},
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
       (_) => {}
     );
 
     return {
       objects_store,
+      rows,
+      columns,
     };
   },
 });
