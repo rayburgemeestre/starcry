abc1f850055dd1b8656c0a2051b2b004a0195412
Ray Burgemeestre <ray@cppse.nl>
2023-02-12 20:17:04 +0000

    Some tweaks to input/web.js

--- input/web.js
+++ input/web.js
@@ -48,7 +48,8 @@ _ = {
           for (let i = 0; i < dots; i++) {
             let a = left[dots - 1 - i];
             let b = right[i];
-            let line = this.spawn3({'id': 'line'}, a, b);
+            let line = this.spawn3({'id': 'glowing_line'}, a, b);
+            let line2 = this.spawn3({'id': 'line'}, a, b);
           }
         }
       },
@@ -70,25 +71,75 @@ _ = {
       'init': function() {},
       'time': function(t, elapsed, s) {},
     },
+    'circle': {
+      'type': 'circle',
+      'radius': 100,
+      'radiussize': 1.0,
+      'gradient': 'red',
+      'opacity': 1.,
+      'hue': 0.,
+      'rotate': 0.,
+      'blending_type': blending_type.normal,
+      'props': {'spawned': false},
+      'init': function() {},
+      'time': function(t, elapsed, s) {},
+    },
     'line': {
       'type': 'line',
       'blending_type': blending_type.add,
       'gradient': 'white',
       'radius': 0,
       'radiussize': 1,
-      'init': function() {},
+      'init': function() {
+        // TODO: figure out a way that we can inherit the cascade stuff...
+        // let line = this.spawn3({'id': 'line'}, a, b);
+        // for now lets do it all manually
+        // yeah, this isn't going to work well..
+        // let line = this.spawn({'id': 'glowing_line', 'x': this.x, 'y': this.y, 'x2': this.x2, 'y2': this.y2,
+        // 'props': {}});
+      },
+      'time': function(t, e, s, tt) {
+
+      },
+    },
+    'glowing_line': {
+      'type': 'line',
+      'blending_type': blending_type.add,
+      'gradient': 'white',
+      'radius': 0,
+      'radiussize': 20,
+      'opacity': 0.1,
+      'init': function() {
+        // how can we reference the parent.
+      },
       'time': function(t, e, s, tt) {},
     },
   },
+  // 'video': {
+  //   'bg_color': {'r': 0., 'g': 0., 'b': 0., 'a': 1},
+  //   'max_intermediates': 10,
+  // },
+  // 'scenes': [
+  //   {
+  //     'name': 'scene',
+  //     'duration': 10.0,
+  //     'objects': [
+  //       {'id': 'line_of_dots', 'x': 0, 'y': 0, 'z': 0, 'props': {}},
+  //     ]
+  //   },
+  // ]
   'video': {
     'bg_color': {'r': 0., 'g': 0., 'b': 0., 'a': 1},
     'max_intermediates': 10,
+    'scale': 1.00,
   },
   'scenes': [
     {
       'name': 'scene',
       'duration': 10.0,
       'objects': [
+        // {'id': 'circle', 'x': 0, 'y': 0, 'z': 0, 'radius': 10, 'props': {}},
+        // {'id': 'circle', 'x': 0, 'y': 0, 'z': 0, 'props': {}},
         {'id': 'line_of_dots', 'x': 0, 'y': 0, 'z': 0, 'props': {}},
       ]
     },
