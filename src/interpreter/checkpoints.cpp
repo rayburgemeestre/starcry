f1b0a88b7c5d551cb7be5c14fc0ad12a2d5317ed
Ray Burgemeestre <ray@cppse.nl>
2024-01-28 08:28:35 +0100

    Add checkpoints class (forgot to add)

---
+++ src/interpreter/checkpoints.cpp
@@ -0,0 +1,44 @@
+/*
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include "checkpoints.h"
+#include "generator.h"
+
+namespace interpreter {
+
+checkpoints::checkpoints(generator& gen) : gen_(gen) {}
+
+void checkpoints::set_checkpoints(std::set<int>& checkpoints) {
+  checkpoints_ = checkpoints;
+}
+
+std::map<int, scenes>& checkpoints::scenes() {
+  return checkpoint_data_scenes_;
+}
+
+std::map<int, data::job>& checkpoints::job() {
+  return checkpoint_data_job_;
+}
+
+std::set<int>& checkpoints::defined() {
+  return checkpoints_;
+}
+
+std::set<int>& checkpoints::available() {
+  return checkpoints_available_;
+}
+
+void checkpoints::insert(data::job& job, interpreter::scenes& scenes) {
+  if (checkpoints_.contains(job.frame_number) && !checkpoint_data_scenes_.contains(job.frame_number)) {
+    // checkpoint this one
+    auto cloned = scenes.clone();
+    checkpoint_data_scenes_.emplace(job.frame_number, std::move(cloned));
+    checkpoint_data_job_.emplace(job.frame_number, job);
+    checkpoints_available_.insert(job.frame_number);
+  }
+}
+
+}  // namespace interpreter
---
+++ src/interpreter/checkpoints.h
@@ -0,0 +1,39 @@
+/*
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+#pragma once
+
+#include <map>
+#include <set>
+
+#include "scenes.h"
+
+namespace data {
+struct job;
+}
+
+namespace interpreter {
+class generator;
+
+class checkpoints {
+public:
+  explicit checkpoints(generator& gen);
+  void set_checkpoints(std::set<int>& checkpoints);
+  std::map<int, scenes>& scenes();
+  std::map<int, data::job>& job();
+  std::set<int>& defined();
+  std::set<int>& available();
+  void insert(data::job& job, interpreter::scenes& scenes);
+
+private:
+  generator& gen_;
+
+  std::set<int> checkpoints_;
+  std::set<int> checkpoints_available_;
+  std::map<int, interpreter::scenes> checkpoint_data_scenes_;
+  std::map<int, data::job> checkpoint_data_job_;
+};
+
+}  // namespace interpreter
