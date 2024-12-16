1072106f2e5b63d8d62bc25cfb53e8d3528d96ad
Ray Burgemeestre <ray@cppse.nl>
2024-12-16 22:38:25 +0100

    Run formatter

--- src/data/texture.hpp
+++ src/data/texture.hpp
@@ -176,7 +176,23 @@ struct texture {
 
   template <class Archive>
   void serialize(Archive &ar) {
-    ar(type, effect, size, octaves, persistence, percentage, scale, fromX, begin, end, toX, strength, speed, m, n, rho, theta);
+    ar(type,
+       effect,
+       size,
+       octaves,
+       persistence,
+       percentage,
+       scale,
+       fromX,
+       begin,
+       end,
+       toX,
+       strength,
+       speed,
+       m,
+       n,
+       rho,
+       theta);
   }
 };
 
