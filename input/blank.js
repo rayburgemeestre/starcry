48b7ae2adb6849b1d6390cf20b9518d46843e8d5
Ray Burgemeestre <ray@cppse.nl>
2024-12-20 14:13:19 +0000

    Add minimal blank.js that renders to nothing for one minute.
    Useful for benchmarking.

---
+++ input/blank.js
@@ -0,0 +1,12 @@
+script = {
+  'gradients': {},
+  'objects': {},
+  'video': {
+    'fps': 25,
+    'width': 1920,
+    'height': 1080,
+  },
+  'scenes': [
+    {'name': 'scene1', 'duration': 60.0, 'objects': []},
+  ]
+}
