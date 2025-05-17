9cd6a3de03991959785c62c72ae17dad5e65d2b4
Ray Burgemeestre <ray@cppse.nl>
2025-05-17 20:56:13 +0000

    Make port 9xxx redir to 18080

--- web/src/components/api.ts
+++ web/src/components/api.ts
@@ -63,7 +63,7 @@ export class StarcryAPI {
       this.ws = new WebSocket(
         protocol +
           '//' +
-          document.location.host.replace(':8080', ':18080').replace(':9000', ':18080') +
+          document.location.host.replace(':8080', ':18080').replace(/:(9\d{3})/g, ':18080') +
           '/' +
           this.endpoint,
         [this.client_data['ID']]
